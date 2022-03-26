0f58b6e44985b3791d3da83991bc616909e81cb2
last-byte <fed.lag@protonmail.ch>
2022-03-26 23:52:47 +0100

    swapped sigthief with carboncopy once more :)

--- postbuild.bat
+++ postbuild.bat
@@ -1,6 +1,6 @@
 cd %0\..\
 python3.exe .\restore_headers.py
-python3.exe .\sigthief.py -i C:\Windows\System32\services.exe -t .\x64\Release\RIPPL_unsigned.exe -o .\x64\Release\RIPPL.exe
+python3.exe .\carboncopy.py www.crowdstrike.com 443 .\x64\Release\RIPPL_unsigned.exe .\x64\Release\RIPPL.exe
 del .\x64\Release\RIPPL_unsigned.exe
 del .\x64\Release\RIPPLDLL_unencrypted.dll
 del .\x64\Release\RIPPLDLL.dll
